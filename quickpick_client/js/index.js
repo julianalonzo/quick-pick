4ffaac8f457bd52519c7d8e87342457715acdae4
julianaubrey <2161495@slu.edu.ph>
2018-06-15 02:20:10 +0800

    Added view food

--- quickpick_client/js/index.js
+++ quickpick_client/js/index.js
@@ -7,7 +7,7 @@ foodRequest.onreadystatechange = function() {
 
         console.log(response);
 
-        // TODO generate food layout here
+        loadUlams(response.canteens);
     }
 }
 
@@ -39,3 +39,31 @@ $('#datepicker').datepicker({
         window.location.href = './index.html?date=' + date + ' 00:00:00';
     }
 });
+
+// Generate ulams
+function loadUlams(canteens) {
+    const menuContainer = document.getElementById('menuContainer');
+
+    for (let i = 0; i < canteens.length; i++) {
+        const foodPlace = canteens[i].name;
+
+        // Name of foodplace
+        const foodPlaceText = document.createTextNode(foodPlace);
+
+        const menuItem = document.createElement('LI');
+        menuItem.appendChild(foodPlaceText);
+
+        // Ulams that will be served
+        const foodListContainer = document.createElement('UL');
+        for (let j = 0; j < canteens[i].foods.length; j++) {
+            const foodNameText = document.createTextNode(canteens[i].foods[j].food_name);
+            const foodItem = document.createElement('LI');
+            foodItem.appendChild(foodNameText);
+            
+            foodListContainer.appendChild(foodItem);
+        }
+
+        menuContainer.appendChild(menuItem);
+        menuContainer.appendChild(foodListContainer);
+    }
+}
