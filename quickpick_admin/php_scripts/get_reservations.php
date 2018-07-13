67c5cd08738c633375784d6e7e56bc6027e96bd0
dawncundangan <2161487@slu.edu.ph>
2018-07-13 09:06:51 +0800

    Reservation

---
+++ quickpick_admin/php_scripts/get_reservations.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+
+    require('connect.php');
+
+    if (isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+
+        $query =    'SELECT dashboard_id, ' .
+                    'food_id, food.username, food_name, price, ' .
+                    'reservation_id, reservation.username, display_name ' .
+                    'FROM food INNER JOIN dashboard USING(food_id) ' .
+                    'INNER JOIN reservation USING(dashboard_id) ' .
+                    'INNER JOIN account ON ' .
+                    'reservation.username= account.username; ' .
+                    'WHERE food.username = ?;';
+
+        $statement = $conn->prepare($query);
+
+        $statement->bind_param('s', $food.username);
+
+        $statement->execute();
+
+        $result = $statement->get_result();
+
+        $foods = array();
+
+        while ($row = $result->fetch_assoc()) {
+            $foods[] = $row;
+        }
+
+        $statement->close();
+
+        echo json_encode($foods);
+    } else {
+        Header('Location: ../reservation.html');
+    }
